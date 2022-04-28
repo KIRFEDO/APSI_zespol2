5367c8e0a7ecd9304ff30dfb6f765b0607282aa8
Shrimpey <kabat97@gmail.com>
2022-04-28 20:17:00 +0200

    Fixing naming conventions, versioning problems, updated gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import  LoginManager
+from flask_login import LoginManager
 from app.config import Config
 
 app = Flask(__name__)
--- database.sh
+++ database.sh
@@ -1,6 +1,6 @@
-CREATE USER aspi with encrypted password '12345';
-DROP DATABASE IF EXISTS aspi_db;
-CREATE DATABASE aspi_db;
-\c aspi_db;
-\i aspi_db.sql;
-ALTER DATABASE aspi_db OWNER TO aspi;
+CREATE USER apsi with encrypted password '12345';
+DROP DATABASE IF EXISTS apsi_db;
+CREATE DATABASE apsi_db;
+\c apsi_db;
+\i apsi_db.sql;
+ALTER DATABASE apsi_db OWNER TO apsi;

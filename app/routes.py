34bbec1deba5b0e4a6450fc8a829d0b250a16edd
Shrimpey <kabat97@gmail.com>
2022-05-14 19:52:15 +0200

    Added quick way to omit logging-in, for development purposes only

--- app/routes.py
+++ app/routes.py
@@ -5,60 +5,57 @@ from app.forms import LoginForm, RegistrationForm, ProjectForm, TaskForm
 from app.models import User, Project, Task
 
 
-# Home
-# @app.route('/')
-# def home():
-#    user = -1
-#    if current_user.is_authenticated:
-#        user = current_user.get_id()
-#    u = User.query.get(user)
-#    return render_template('home.html', u=u)
-
-
-# Employee views ---------------------------------------------------------------
-
-# Taskboard list
-@app.route('/taskboard')
-def taskboard():
-    current_view = 'taskboard'
+# Temp variable for skipping login process
+login_override = True
+def check_user():
+    if login_override:
+        return User.query.filter(User.role == 'kierownik').first()
     user = -1
     if current_user.is_authenticated:
         user = current_user.get_id()
-    u = User.query.get(user)
+    return User.query.get(user)
+
+
+# Taskboard list
+@app.route('/taskboard')
+#@login_required    # TODO: Uncomment in final
+def taskboard():
+    current_view = 'taskboard'
+    u = check_user()
 
     return render_template('common/taskboard.html', u=u, current_view=current_view)
 
 
 # Projects list
 @app.route('/projects')
-@login_required
+#@login_required    # TODO: Uncomment in final
 def projects():
     current_view = 'projects'
-    user = current_user.get_id()
-    u = User.query.get(user)
+    u = check_user()
 
-    if u.role == 'kierownik':
+    if login_override or (u.role == 'kierownik'):
         projectlist = Project.query.filter(Project.supervisor == u.id)
+        if(login_override):
+            projectlist = Project.query.all()
         return render_template('admin/admin-project-list.html', projects=projectlist, u=u, current_view=current_view)
-
     else:
         return render_template('employee/employee-project-list.html', u=u, current_view=current_view)
 
 
 # Projects add
 @app.route('/projects/add', methods=['GET', 'POST'])
-@login_required
+#@login_required    # TODO: Uncomment in final
 def project_add():
     form = ProjectForm()
     current_view = 'projects-add'
-    user = current_user.get_id()
-    u = User.query.get(user)
+    u = check_user()
 
-    if u.role == 'kierownik':
+    if login_override or (u.role == 'kierownik'):
         if form.validate_on_submit():
             c = form.client.data
             if c == "":
                 c = None
+            user = current_user.get_id()
             project = Project(name=form.name.data, description=form.description.data, supervisor=user,
                               client=c)
             db.session.add(project)
@@ -72,43 +69,30 @@ def project_add():
 
 # Single project
 @app.route('/projects/<int:project_id>')
-@login_required
+#@login_required    # TODO: Uncomment in final
 def projects_view(project_id):
     project = Project.query.get_or_404(project_id)
     current_view = 'projects-view'
-    user = current_user.get_id()
-    u = User.query.get(user)
+    u = check_user()
 
-    if u.role == 'kierownik':
+    if login_override or (u.role == 'kierownik'):
         return render_template('admin/admin-project-view.html', u=u, project=project, current_view=current_view)
 
     else:
         return render_template('employee/employee-project-view.html', u=u, project=project, current_view=current_view)
 
 
-# Add new task          - stary task, zmiana na activity
-# @app.route('/task/add')
-# def addtask():
-#     current_view = 'task-add'
-#     user = -1
-#     if current_user.is_authenticated:
-#         user = current_user.get_id()
-#     u = User.query.get(user)
-#     return render_template('common/task-add.html', u=u)
-
-
 # Add new task
 @app.route('/projects/<int:project_id>/task/add', methods=['GET', 'POST'])
-@login_required
+#@login_required    # TODO: Uncomment in final
 def task_add(project_id):
     form = TaskForm()
     current_view = 'task-add'
-    user = current_user.get_id()
-    u = User.query.get(user)
+    u = check_user()
     project = Project.query.get_or_404(project_id)
     form.project_id = project.id
 
-    if u.role == 'kierownik':
+    if login_override or (u.role == 'kierownik'):
         if form.validate_on_submit():
             task = Task(name=form.name.data, description=form.description.data, project=project_id)
             db.session.add(task)
@@ -121,16 +105,14 @@ def task_add(project_id):
         abort(403)
 
 
-# Single project
+# Employee list
 @app.route('/employees')
+#@login_required    # TODO: Uncomment in final
 def employees():
     current_view = 'employees'
-    user = -1
-    if current_user.is_authenticated:
-        user = current_user.get_id()
-    u = User.query.get(user)
+    u = check_user()
 
-    if (u.role == 'kierownik'):
+    if login_override or (u.role == 'kierownik'):
         employeeList = User.query.all()
         return render_template('admin/admin-employee-list.html', employees=employeeList, u=u, current_view=current_view)
 
@@ -139,7 +121,7 @@ def employees():
         return 0
 
 
-# Login/registration -----------------------------------------------------------
+# Registration -----------------------------------------------------------------
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -154,6 +136,7 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
+# Login ------------------------------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():

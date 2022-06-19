e6ebcda246ecb3f2cbad56c91b6f25f09a5ea903
miecielicas <s.miecielica@stud.elka.pw.edu.pl>
2022-06-19 23:00:36 +0200

    Fix add project button permissions for users

--- app/routes.py
+++ app/routes.py
@@ -344,7 +344,7 @@ def project_add():
     current_view = 'projects-add'
     u = get_user()
 
-    if u.role == 'kierownik' or u.role == 'pracownik':
+    if u.role == 'kierownik':
         if form.validate_on_submit():
             c = form.client.data
             if c == "":
